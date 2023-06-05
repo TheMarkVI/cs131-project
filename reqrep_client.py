2c630d269efe3c99af3561ef999b1452a620e13d
Arnav-Menon <arnavmenon@gmail.com>
2023-06-04 19:01:48 -0700

    trying something

--- reqrep_client.py
+++ reqrep_client.py
@@ -1,7 +1,7 @@
 import zmq
 import sys
 
-port = "5556"
+port = "5679"
 if len(sys.argv) > 1:
     port =  sys.argv[1]
     int(port)
@@ -13,12 +13,14 @@ if len(sys.argv) > 2:
 context = zmq.Context()
 print("Connecting to server...")
 socket = context.socket(zmq.REQ)
-socket.connect ("tcp://localhost:%s" % port)
+# socket.connect("tcp://themarkvi.asuscomm.com:5679")
+# socket.connect("tcp://97.94.97.230:5679")
+socket.connect("tcp://10.13.240.1:2000")
 if len(sys.argv) > 2:
-    socket.connect ("tcp://localhost:%s" % port1)
+    socket.connect("tcp://97.94.97.230:%s" % port1)
 
 #  Do 5 requests, waiting each time for a response
-for request in range (5):
+for request in range(5):
     print("Sending request ", request,"...")
     socket.send_string("Hello")
     #  Get the reply.
--- reqrep_server.py
+++ reqrep_server.py
@@ -2,27 +2,45 @@ import zmq
 import time
 import sys
 
-port = "5556"
+port = "5679"
 if len(sys.argv) > 1:
     port =  sys.argv[1]
     int(port)
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-socket.bind("tcp://*:%s" % port)
+# socket.bind("tcp://themarkvi.asuscomm.com:5679")
+# socket.bind("tcp://127.0.0.1:%s" % port)
+# socket.bind("tcp://10.13.240.1:%s" % port)
+socket.bind("tcp://0.0.0.0:2000")
 print("Connected on port", port)
 
-num_iters = 0
-
 while True:
-    #  Wait for next request from client
-    print("Waiting for message...")
-    message = socket.recv()
-    # close connection
-    if message == b"Bye":
-        print("Closing connection!")
-        break
-    print("\tReceived request: ", message)
-    time.sleep (1)  
-    socket.send_string("World from %s" % port)
-    num_iters += 1
+    try:
+        #  Wait for next request from client
+        print("Waiting for message...")
+        message = socket.recv()
+        # close connection
+        if message == b"Bye":
+            print("Closing connection!")
+            break
+        print("\tReceived request: ", message)
+        time.sleep(1)  
+        socket.send_string("World from %s" % port)
+    except Exception as e:
+        print(e)
+
+
+# import requests
+
+# try:
+#     x = requests.get("https://97.94.97.230", timeout=10)
+#     print(x.status_code)
+# except requests.exceptions.ConnectionError as e:
+#     raise ValueError("Firewall is blocking the connection")
+
+
+# have siraaj on ucr wifi
+# ip should be 10.13.240.1
+# bind? and connect to that ip
+# client connects and should be able to send messages like that
