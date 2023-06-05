7c11967ec29edf0a6465e45317ff072dc2b8d2dc
TheMarkVI <arnak9167@gmail.com>
2023-06-04 17:50:38 -0700

    Add prints, modify net model
    - Temporarily set net model to default
    - Rename NETPORT to PORT
    - add print lines to end of program

--- fridgeDetect.py
+++ fridgeDetect.py
@@ -5,6 +5,10 @@ https://github.com/dusty-nv/jetson-inference/blob/master/docs/detectnet-example-
 
 '''
 import numpy as np
+#import zmq
+import time
+import sys
+
 from jetson_inference import detectNet
 from jetson_utils import videoSource, videoOutput
 
@@ -15,13 +19,19 @@ from jetson_utils import videoSource, videoOutput
 #                 input_blob="input_0", output_cvg="scores", output_bbox="boxes", 
 #                 threshold=args.threshold)
 
-MODELPATH = "~chocogoats/jetson-inference/python/training/detection/ssd/models/fridge/ssd-mobilenet.onnx"
+PORT = 5679
+
+MODELPATH = "../jetson-inference/python/training/detection/ssd/models/fruit/ssd-mobilenet.onnx"
+LABELPATH = "../jetson-inference/python/training/detection/ssd/models/fruit/labels.txt"
 
 # load objection detection model 
-# net = detectNet("ssd-mobilenet-v2", threshold=0.5) # Uses the default ssd-mobilenet-v2 model
-net = detectNet(model=MODELPATH, labels="model/fridge/labels.txt", \
-                input_blob="input_0", output_cvg="scores", output_bbox="boxes", threshold=0.5)
-                # Uses retrained model with fridge objects
+net = detectNet("ssd-mobilenet-v2", threshold=0.5) # Uses the default ssd-mobilenet-v2 model
+# net = detectNet(model=MODELPATH, labels=LABELPATH, \
+#                input_blob="input_0", output_cvg="scores", output_bbox="boxes", threshold=0.5)
+                		     # Uses retrained model with fridge objects
+				     # Might be able to move the model and label paths
+                                     # in the same directory. Just .onnx and .txt files
+
 camera = videoSource("/dev/video0")  # 'csi://0' for MIPI CSI camera
 display = videoOutput("display://0") # 'my_video.mp4' for file
 
@@ -38,29 +48,37 @@ itemsFound = [] # list of items found
 
 # Load labels from labels.txt file
 fridgeList = []
-with open("models/fridge/labels.txt", "r") as f:
+with open(LABELPATH, "r") as f:
     for line in f:
         fridgeList.append(line.strip())
+
+print("Labels:", fridgeList)
 
 # print(fridgeList) # verify: print labels from labels.txt file
 
 # driver code for object detection
+input("Press Enter to Continue...")
+
 while display.IsStreaming():
     img = camera.Capture()
 
     if img is not None:
+        # print("render image...")
+        #print("itemsNeeded:", itemsNeeded)
+        #print("itemsFound:", itemsFound)
         continue
 
     detections = net.Detect(img)
+    print(detections)
 
     display.Render(img)
     display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
 
     # not sure if we need this...
-    print("detected {:d} objects in image".format(len(detections))) # print number of objects detected
+    # print("detected {:d} objects in image".format(len(detections))) # print detections
 
-    for detection in detections:
-        print(detection) # print object name, confidence, bounding box coordinates
+    # for detection in detections:
+    #    print(detection) # print object name, confidence, bounding box coordinates
 
     # To do:
     # Implement a way to check if the object is in the fridge
@@ -72,4 +90,6 @@ while display.IsStreaming():
     # To do: send itemsNeeded to a server
     # via MQTT or something similar
 
-
+print("fridgeList:", fridgeList)
+print("itemsNeeded:", itemsNeeded)
+print("itemsFound:", itemsFound)
